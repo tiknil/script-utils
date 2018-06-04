3cb17e4dd88643863d5469ea4364f7fae59127da
Tribbia Riccardo <tribbia.riccardo@tiknil.com>
2018-06-04 17:39:27 +0200

    - Implementato script per importare / exportare da excel a twine e viceversa

--- twine2xls.rb
+++ twine2xls.rb
@@ -13,6 +13,13 @@
 
 $version = '0.0.1'
 $year = '2018'
+$section_background_color = 'a3fff5'
+#definizione lingue gestite
+$languages = {
+  "it" => {:desc => "Italiano (it)", :index => 1},
+  "en" => {:desc => "Inglese (en)", :index => 2},
+  "pl" => {:desc => "Polacco (pl)", :index => 3}
+}
 
 ############################################################################################################################################
 ## MAIN PROGRAM
@@ -31,6 +38,8 @@ begin
   require 'tempfile'          # per creare dei file temporanei in memoria
   require 'ruby-progressbar'  # per le progress bar nel terminale
   require 'tty-spinner'       # per gli spinner indefiniti nel terminale
+  require 'rubygems'          # dipendenza di rubyXL
+  require 'rubyXL'            # per leggere e scrivere i file xlsx
 rescue LoadError
   # Installazione dipendenze
   puts "Installazione dipendenze mancanti in corso..."
@@ -50,14 +59,132 @@ end
 
 class Twine2xls < Thor
 
-  desc "export", "Esporta il file twine in xsl"
+  desc "export", "Esporta il file twine in xslx"
+  option :input, :required => true, :type => :string, :desc => 'Il file twine da esportare', :aliases => '-i'
+  option :output, :default => 'output.xlsx', :type => :string, :desc => 'Il percorso del file Excel di output', :aliases => '-o'
   def export
+    #puts "args: #{options[:input]}, #{options[:output]}"
 
+    if !File.exist? options[:input]
+      abort "Il file #{options[:input]} non esiste"
+    end
+
+    if File.exist? options[:output]
+      puts "Il file #{options[:output]} esiste già, sovrascriverlo? (Y/n)"
+      confirm = STDIN.gets.chomp
+      if confirm != "Y"
+        abort
+      end
+    end
+
+    # Predispongo l'oggetto Excel
+    workbook = RubyXL::Workbook.new
+    worksheet = workbook[0]
+    worksheet.sheet_name = 'Traduzioni'
+
+    #Prima colonna, CHIAVE
+
+    #Headers
+    worksheet.add_cell 0,0,"Keys"
+
+    $languages.each do |key, value|
+      worksheet.add_cell 0, value[:index] , value[:desc]
+      worksheet.change_column_width(value[:index], 100)
+    end
+
+    row_index = 0
+
+    #worksheet.change_column_fill(0, 'e7fd55') #Sfondo colonna chiavi
+    worksheet.change_row_bold(0, true)
+    worksheet.change_row_border(0, :bottom, 'medium')
+
+    spinner = TTY::Spinner.new(":spinner Parsing del file twine in corso... \r", :clear => true, :hide_cursor => true)
+    spinner.auto_spin
+
+    File.open(options[:input], 'r') do |file|
+      file.each_line do |line|
+        trimmed_line = line.strip
+        if trimmed_line.start_with? "[["
+          #Section
+          section = trimmed_line.gsub('[','').gsub(']','')
+          row_index += 1
+          worksheet.add_cell row_index, 0, section
+          worksheet.change_row_fill(row_index, $section_background_color)
+          worksheet.sheet_data[row_index][0].change_font_bold(true)
+          worksheet.merge_cells(row_index, 0, row_index, 5)
+        elsif trimmed_line.start_with? "["
+          #Key
+          key = trimmed_line.gsub('[','').gsub(']','')
+          row_index += 1
+          worksheet.add_cell row_index, 0, key
+        else
+          #Localizations
+          elements = trimmed_line.split " = "
+          found = false
+          if elements.length > 0
+            $languages.each do |key, value|
+              if elements[0].strip == key
+                found = true
+                worksheet.add_cell row_index, value[:index], elements[1]
+              end
+            end
+          end
+
+          if !found
+            row_index += 1
+          end
+        end
+      end
+    end
+
+    # Scrivo il file Excel nel percorso di output
+    workbook.write(options[:output])
+    spinner.success
   end
 
-  desc "import", "Importa il file xls nel file twine"
+  desc "import", "Importa il file xlsx nel file twine"
+  option :input, :required => true, :type => :string, :desc => 'Il file xlsx da importare in twine', :aliases => '-i'
+  option :output, :default => 'output.txt', :type => :string, :desc => 'Il percorso del file twine di output', :aliases => '-o'
   def import
+    puts "args: #{options[:input]}, #{options[:output]}"
 
+    if !File.exist? options[:input]
+      abort "Il file #{options[:input]} non esiste"
+    end
+
+    if File.exist? options[:output]
+      puts "Il file #{options[:output]} esiste già, sovrascriverlo? (Y/n)"
+      confirm = STDIN.gets.chomp
+      if confirm != "Y"
+        abort
+      end
+    end
+
+    workbook = RubyXL::Parser.parse(options[:input])
+    worksheet = workbook.worksheets[0]
+
+    File.open(options[:output], 'w') do |out|
+      worksheet.each { |row|
+        if row.index_in_collection > 0
+          if row.size == 0
+            out << "\n"
+          elsif row.size == 1
+            out << "[[#{row[0].value}]]\n"
+          else
+            if row[0] == nil || row[0].value == nil || row[0].value.to_s.empty?
+              out << "\n"
+            else
+             out << "\t[#{row[0].value}]\n"
+             $languages.each do |key, value|
+               if row[value[:index]] != nil
+                 out << "\t\t#{key} = #{row[value[:index]].value}\n"
+               end
+             end
+           end
+          end
+        end
+      }
+    end
   end
 
 end
